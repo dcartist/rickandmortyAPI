1ab808361adac988cbb72b5123ce3a714b9cc124
DCartist <pbannerman13@gmail.com>
2019-09-07 14:20:15 -0400

    Working on the ID

--- controllers/gladiators.js
+++ controllers/gladiators.js
@@ -23,8 +23,8 @@ router.get("/score/:score", (req, res) => {
     let theName = req.params.score
     Gladiator.find({ 'results.score': theName }).then(showName => res.json(showName))
 })
-router.get("/id/:id", (req, res) => {
-    let theName = req.params.id
+router.get("/id/:_id", (req, res) => {
+    let theName = req.params._id
     Gladiator.find({ 'results._id': theName }).then(showName => res.json(showName))
 })
 router.get("/gender/:gender", (req, res) => {
